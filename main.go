4c671c12d835659db06d152eb53dde9ecbda8a61
saschel88 <as@crypta.kz>
2018-09-21 19:35:01 +0600

    НАчало

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	pathName := "./testdata"
+	//dirName:=""
+	files, err := ioutil.ReadDir(pathName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+
+} // the end main
